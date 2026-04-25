e42739bbd938e0b770e99a9bbd0a4894aadc0128
vondoglok <kapchik30@gmail.com>
2026-04-26 00:16:55 +0300

    Create 175_Combine_Two_Tables.sql

---
+++ easy/175_Combine_Two_Tables.sql
@@ -0,0 +1,13 @@
+-- LeetCode #175: Combine Two Tables
+-- Difficulty: Easy
+--
+-- Table: Person (personId, lastName, firstName)
+-- Table: Address (addressId, personId, city, state)
+--
+-- Task: Write a query to report the first name, last name, city, and state 
+-- of each person. If a person does not have an address, show city and state as NULL.
+--
+-- Solution:
+SELECT P.firstName, P.lastName, A.city, A.state
+FROM Person P
+LEFT JOIN Address A ON P.personId = A.personId;
