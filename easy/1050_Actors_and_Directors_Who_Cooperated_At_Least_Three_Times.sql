3260422fbdf79501ca403b3c1be2954042d5822f
vondoglok <kapchik30@gmail.com>
2026-05-13 22:07:59 +0300

    Create 1050_Actors_and_Directors_Who_Cooperated_At_Least_Three_Times.sql

---
+++ easy/1050_Actors_and_Directors_Who_Cooperated_At_Least_Three_Times.sql
@@ -0,0 +1,12 @@
+-- LeetCode #1050: Actors and Directors Who Cooperated At Least Three Times
+-- Difficulty: Easy
+--
+-- Table: ActorDirector (actor_id, director_id, timestamp)
+--
+-- Task: Write a solution to find all the pairs (actor_id, director_id) where the actor has cooperated with the director at least three times.
+--
+-- Runtime: 395ms
+SELECT actor_id, director_id
+FROM ActorDirector
+GROUP BY actor_id, director_id
+HAVING COUNT(*) >= 3;
