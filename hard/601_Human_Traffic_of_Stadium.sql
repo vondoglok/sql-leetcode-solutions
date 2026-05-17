c3eb09a60e0b70f1ec9d03bfc75dcb35661bd113
vondoglok <kapchik30@gmail.com>
2026-05-18 00:18:15 +0300

    Create 601_Human_Traffic_of_Stadium.sql

---
+++ hard/601_Human_Traffic_of_Stadium.sql
@@ -0,0 +1,23 @@
+-- LeetCode #601: Human Traffic of Stadium
+-- Difficulty: Hard
+--
+-- Tables: Stadium (id, visit_date, people)
+--
+-- Task: Write a solution to display the records with three or more rows with consecutive id's, 
+-- and the number of people is greater than or equal to 100 for each.
+--
+-- Runtime: 368 ms
+WITH Flagged AS (
+    SELECT id, visit_date, people,
+    id - ROW_NUMBER () OVER (ORDER BY id) AS grp
+    FROM Stadium
+    WHERE people >= 100
+),
+Grouped AS (
+    SELECT *, COUNT(*) OVER (PARTITION BY grp) AS group_rank
+    FROM Flagged
+)
+SELECT id, visit_date, people
+FROM Grouped
+WHERE group_rank >= 3
+ORDER BY visit_date;
